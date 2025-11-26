ec4e46292d7c901db150c393d6d2727142715247
Zachariah <12345678+Shadowyfigurings87@users.noreply.github.com>
2025-11-25 20:38:22 -0500

    commited

--- subsystem/serial_forwarder.py
+++ subsystem/serial_forwarder.py
@@ -1,12 +1,10 @@
+from gui.arduino import serial_queue
 import socket, time
-from gui.arduino import get_serial
 
 HOST = "0.tcp.ngrok.io"
 PORT = 12958
 
 def run_forwarder():
-    ser = get_serial()
-
     def connect_socket():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((HOST, PORT))
@@ -17,10 +15,9 @@ def run_forwarder():
 
     while True:
         try:
-            line = ser.readline().decode().strip()
-            if line:
-                s.sendall((line + "\n").encode())
-                print(f"Forwarded Arduino → Host: {line}")
+            line = serial_queue.get()   # blocks until data available
+            s.sendall((line + "\n").encode())
+            print(f"Forwarded Arduino → Host: {line}")
         except (BrokenPipeError, ConnectionResetError):
             print("Connection lost, reconnecting...")
             time.sleep(2)
