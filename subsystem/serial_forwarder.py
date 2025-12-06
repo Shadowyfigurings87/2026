bcfaf3ae7391e9139b4c64df90e4ef49039601cb
Zachariah <123456+Shadowyfigurings87@users.noreply.github.com>
2025-12-06 17:50:29 -0500

    commit

--- subsystem/serial_forwarder.py
+++ subsystem/serial_forwarder.py
@@ -1,8 +1,8 @@
 import socket, time, threading
 from gui.arduino import get_queue, get_serial
 
-HOST = "8.tcp.ngrok.io"
-PORT = 10520
+HOST = "0.tcp.ngrok.io"
+PORT = 17013
 
 def run_forwarder():
     q = get_queue()       # consume Arduino lines from queue
